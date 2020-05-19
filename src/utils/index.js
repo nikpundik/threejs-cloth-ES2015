a0b87b5b03b1fae48abb8b429650f1cd891a75ca
nik <nicola.gnecchi@gmail.com>
2020-05-19 16:12:16 +0200

    fixed plane util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,11 @@
 export default {
   plane(width, height) {
-    return (u, v, vector) => {
-      vector.x = (u - 0.5) * width;
-      vector.y = (v + 0.5) * height;
-      vector.z = 0;
-      return vector;
+    return (u, v, target) => {
+      const x = (u - 0.5) * width;
+      const y = (v + 0.5) * height;
+      const z = 0;
+      target.set(x, y, z);
+      return target;
     };
   },
 };
